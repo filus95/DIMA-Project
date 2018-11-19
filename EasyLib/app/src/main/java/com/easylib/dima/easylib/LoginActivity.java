9e290c32444711589198f36d9a13cd2dfe807bbc
Kostandin Caushi <koci4@live.it>
2018-11-19 12:52:32 +0100

    Login Activity Testing for Multiple Screens

--- EasyLib/app/src/main/java/com/easylib/dima/easylib/LoginActivity.java
+++ EasyLib/app/src/main/java/com/easylib/dima/easylib/LoginActivity.java
@@ -2,12 +2,15 @@ package com.easylib.dima.easylib;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.login);
     }
 }
