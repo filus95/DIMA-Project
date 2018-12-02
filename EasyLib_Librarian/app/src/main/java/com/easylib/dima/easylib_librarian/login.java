578ec760aee9c45b96e4d81fa98f4bb605dacb9f
Kostandin Caushi <koci4@live.it>
2018-12-03 00:12:30 +0100

    [ELL] Done Login Screen for Normal
    + added compenents
    + added images

--- EasyLib_Librarian/app/src/main/java/com/easylib/dima/easylib_librarian/login.java
+++ EasyLib_Librarian/app/src/main/java/com/easylib/dima/easylib_librarian/login.java
@@ -2,6 +2,8 @@ package com.easylib.dima.easylib_librarian;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class login extends AppCompatActivity {
 
@@ -9,5 +11,23 @@ public class login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+    }
+
+    public void login(View view) {
+        // Get username & password
+        EditText uText = (EditText) findViewById(R.id.username);
+        String username = uText.getText().toString();
+        EditText pText = (EditText) findViewById(R.id.password);
+        String password = pText.getText().toString();
+
+        //TODO: Make the call to Server
+    }
+
+    public void loginFb(View view) {
+        //TODO: call Facebook login API
+    }
+
+    public void loginGoogle(View view) {
+        //TODO: call Google login API
     }
 }
