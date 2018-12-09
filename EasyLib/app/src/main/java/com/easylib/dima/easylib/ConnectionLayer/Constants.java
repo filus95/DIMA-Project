54d514d54dcdc26dce8e5c2aaecc49bdda3623f4
Raffaele Bongo <raffaele.bongo@mail.polimi.it>
2018-12-09 14:04:10 +0100

    - file of communication constants added

---
+++ EasyLib/app/src/main/java/com/easylib/dima/easylib/ConnectionLayer/Constants.java
@@ -0,0 +1,33 @@
+package com.easylib.dima.easylib.ConnectionLayer;
+
+
+/**
+ *  Constants
+ */
+public class Constants {
+
+    static final String GET_USER_RESERVATION = "get_user_reservation";
+    static final String GET_EVENTS = "get_events";
+    static final String GET_NEWS = "get_news";
+    static final String GET_WAITING_LIST_BOOK = "get_waiting_list_book";
+    static final String GET_USER_PREFERENCES = "get_user_preferences";
+    static final String INSERT_RATING = "insert_rating";
+    static final String INSERT_PREFERENCE = "insert_preference";
+    static final String PASSWORD_FORGOT = "password_forgot";
+    static final String USER_LOGIN = "user_login";
+    static final String GET_ALL_LIBRARIES = "get_all_libraries";
+    static final String GET_LIBRARY_INFO = "get_library_info";
+    static final String INSERT_NEW_LIBRARY = "insert_new_library";
+    static final String REGISTER_USER = "register_user";
+    static final String INSERT_WAITING_PERSON = "insert_waiting_person";
+    static final String INSERT_NEWS = "insert_news";
+    static final String QUERY_ON_BOOKS = "query_on_books" ;
+    static final String GET_LIBRARY_CONN_INFO = "library_conn_info";
+    static final String INSERT_RESERVATION = "insert_reservation";
+    static final String INSERT_EVENT_PARTICIPANT = "insert_event_participant";
+    static final String INSERT_EVENT = "insert_event";
+    static final String TEST_CONN = "test";
+    static final String GET_ALL_BOOKS = "get_all_books";
+    // Communication strings
+
+}
