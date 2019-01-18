816dce5811fbb5f2416168cc7ec3de4dd7a14dba
Kostandin Caushi <koci4@live.it>
2019-01-18 18:12:44 +0100

    Added AllLibraries on HomeFragment to Test LibraryListActivity

--- EasyLib/app/src/main/java/com/easylib/dima/easylib/Activities/Fragments/HomeFragment.java
+++ EasyLib/app/src/main/java/com/easylib/dima/easylib/Activities/Fragments/HomeFragment.java
@@ -1,12 +1,15 @@
 package com.easylib.dima.easylib.Activities.Fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.easylib.dima.easylib.Activities.Lists.LibraryListActivity;
 import com.easylib.dima.easylib.R;
 
 
@@ -16,6 +19,17 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        Button button = (Button) root.findViewById(R.id.allLibraries_home_fragment);
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(getContext(), LibraryListActivity.class);
+                startActivity(intent);
+            }
+        });
+        return root;
     }
 }
