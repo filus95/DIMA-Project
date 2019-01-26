7387249e2d416c8f1c42ed3dbe01de07622a3c2e
Kostandin Caushi <koci4@live.it>
2019-01-26 17:36:26 +0100

    [LIB] Created Book Activity
    - activity
    - items for reservations
    - adapter for reservations

---
+++ EasyLib_Librarian/app/src/main/java/com/easylib/dima/easylib_librarian/Activities/BookActivity.java
@@ -0,0 +1,15 @@
+package com.easylib.dima.easylib_librarian.Activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.easylib.dima.easylib_librarian.R;
+
+public class BookActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.library);
+    }
+}
---
+++ EasyLib_Librarian/app/src/main/java/com/easylib/dima/easylib_librarian/Adapters/BookReservationsAdapter.java
@@ -0,0 +1,67 @@
+package com.easylib.dima.easylib_librarian.Adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.easylib.dima.easylib_librarian.R;
+
+import java.util.ArrayList;
+
+import AnswerClasses.User;
+
+public class BookReservationsAdapter extends RecyclerView.Adapter<BookReservationsAdapter.BookAvailableLibHolder> {
+
+    // TODO : decide vhich parameters to pass... need also to know number of user on waiting list for each book
+
+    ArrayList<User> users;
+    Context context;
+
+    public BookReservationsAdapter(Context context, ArrayList<User> users) {
+        this.context = context;
+        this.users = users;
+    }
+
+    @Override
+    public BookAvailableLibHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // inflate the item
+        View v = LayoutInflater.from(context).inflate(R.layout.book_activity_reservation_item, parent, false);
+        return new BookAvailableLibHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(BookAvailableLibHolder holder, int position) {
+        // set the data in items TODO
+        User user = users.get(position);
+        holder.userEmail.setText(user.getEmail());
+
+        // implemented onClickListener event
+        holder.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO : onClick confirm reservation
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return users.size();
+    }
+
+    static class BookAvailableLibHolder extends RecyclerView.ViewHolder {
+        protected Button button;
+        protected TextView userEmail;
+
+        public BookAvailableLibHolder(View v) {
+            super(v);
+            button = v.findViewById(R.id.book_activity_reservation_item_button);
+            userEmail = v.findViewById(R.id.book_activity_reservation_item_name);
+        }
+    }
+}
