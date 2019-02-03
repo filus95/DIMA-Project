082ba48faf3baad6dc84aabd73b2a8e3703dabb9
Kostandin Caushi <koci4@live.it>
2019-02-03 12:24:46 +0100

    Added News link to server

--- EasyLib/app/src/main/java/com/easylib/dima/easylib/Activities/NewsActivity.java
+++ EasyLib/app/src/main/java/com/easylib/dima/easylib/Activities/NewsActivity.java
@@ -1,20 +1,33 @@
 package com.easylib.dima.easylib.Activities;
 
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.easylib.dima.easylib.ConnectionLayer.ConnectionService;
+import com.easylib.dima.easylib.ConnectionLayer.Constants;
 import com.easylib.dima.easylib.R;
 
-import java.time.LocalDateTime;
-import java.time.Month;
+import java.util.Iterator;
+import java.util.Objects;
+import java.util.Set;
 
+import AnswerClasses.LibraryDescriptor;
 import AnswerClasses.News;
 
 public class NewsActivity extends AppCompatActivity {
 
+    private static final String NEWS_INFO = "News Info";
     private News news;
 
     private TextView title;
@@ -23,17 +36,71 @@ public class NewsActivity extends AppCompatActivity {
     private TextView description;
     private ImageView image;
 
+    //Comunication
+    ConnectionService mBoundService;
+    private boolean mIsBound;
+
+    //For the communication Service
+    private ServiceConnection mConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            mBoundService = ((ConnectionService.LocalBinder)service).getService();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mBoundService = null;
+        }
+    };
+
+    public void doBindService() {
+        bindService(new Intent (NewsActivity.this, ConnectionService.class), mConnection,
+                Context.BIND_AUTO_CREATE);
+        mIsBound = true;
+        if(mBoundService!=null){
+            mBoundService.IsBoundable();
+        }
+    }
+
+    public void doUnbindService() {
+        if (mIsBound) {
+            // Detach our existing connection.
+            unbindService(mConnection);
+            mIsBound = false;
+        }
+        unregisterReceiver(mMessageReceiver);
+    }
+
+    private String extractKey(Intent intent){
+        Set<String> keySet = Objects.requireNonNull(intent.getExtras()).keySet();
+        Iterator iterator = keySet.iterator();
+        return (String)iterator.next();
+    }
+
+    //This is the handler that will manager to process the broadcast intent
+    //This has to be created inside each activity that needs it ( almost anyone )
+    private BroadcastReceiver mMessageReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String key = extractKey(intent);
+
+            if (key.equals(Constants.GET_LIBRARY_INFO)) {
+                LibraryDescriptor library = (LibraryDescriptor) intent.getSerializableExtra (Constants.GET_LIBRARY_INFO);
+                location.setText (library.getAddress ());
+            }
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.news_activity);
 
-        // JUST FOR TEST
-        news = new News();
-        news.setImage_link("https://upload.wikimedia.org/wikipedia/en/thumb/7/70/Brisingr_book_cover.png/220px-Brisingr_book_cover.png");
-        news.setTitle("Title Event");
-        news.setContent("skjdnfkjdnf dsjfdsjk fdsf jkds fkjds fkds fksjd f");
-        news.setPost_date(LocalDateTime.of(2014, Month.JANUARY, 1, 10, 10, 30));
+        // Communication
+        doBindService();
+        this.registerReceiver(mMessageReceiver, new IntentFilter (Constants.GET_LIBRARY_INFO));
+
+        news = (News) getIntent ().getSerializableExtra(NEWS_INFO);
 
         // get components
         title = (TextView) findViewById(R.id.news_activity_title);
@@ -44,11 +111,27 @@ public class NewsActivity extends AppCompatActivity {
 
         // set the components
         title.setText(news.getTitle());
-        location.setText("via non la so (MI)");
         date.setText(news.getPost_date().toString());
         description.setText(news.getContent());
         Glide.with(this)
                 .load(news.getImage_link())
                 .into(image);
+
+        // Get user Preferences from Server to see if this library is Prefered
+        new Handler ().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (mBoundService != null) {
+                    mBoundService.setCurrentContext(getApplicationContext());
+                    mBoundService.sendMessage(Constants.GET_LIBRARY_INFO, news.getIdLib ());
+                }
+            }
+        }, 1000);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy ();
+        doUnbindService();
     }
 }
--- EasyLib/app/src/main/java/com/easylib/dima/easylib/Adapters/ImageTitleNewsAdapter.java
+++ EasyLib/app/src/main/java/com/easylib/dima/easylib/Adapters/ImageTitleNewsAdapter.java
@@ -1,6 +1,8 @@
 package com.easylib.dima.easylib.Adapters;
 
 import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +11,9 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.easylib.dima.easylib.Activities.Fragments.MainActivity;
+import com.easylib.dima.easylib.Activities.LibraryActivity;
+import com.easylib.dima.easylib.Activities.NewsActivity;
 import com.easylib.dima.easylib.R;
 
 import java.util.ArrayList;
@@ -17,6 +22,7 @@ import AnswerClasses.News;
 
 public class ImageTitleNewsAdapter extends RecyclerView.Adapter<ImageTitleNewsAdapter.ImageTitleNewsHolder> {
 
+    private static final String NEWS_INFO = "News Info";
     ArrayList<News> newsList;
     Context context;
 
@@ -45,7 +51,15 @@ public class ImageTitleNewsAdapter extends RecyclerView.Adapter<ImageTitleNewsAd
             holder.itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                        // TODO : onClick open Library Activity
+                    Intent newsIntent = new Intent (context, NewsActivity.class);
+                    Bundle bundle = new Bundle ();
+                    bundle.putSerializable(NEWS_INFO, news);
+                    newsIntent.putExtras(bundle);
+                    if(context instanceof MainActivity)
+                        ((MainActivity)context).doUnbindService ();
+                    if(context instanceof LibraryActivity)
+                        ((LibraryActivity)context).doUnbindService ();
+                    context.startActivity(newsIntent);
                         }
             });
     }
