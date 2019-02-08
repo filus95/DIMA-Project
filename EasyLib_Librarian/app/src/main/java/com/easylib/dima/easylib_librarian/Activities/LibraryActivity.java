db4bce11a21a9835c9a3d46602ce7f8cf2e27a20
Kostandin Caushi <koci4@live.it>
2019-02-08 23:35:07 +0100

    [LIB] Updated LibraryActivity

--- EasyLib_Librarian/app/src/main/java/com/easylib/dima/easylib_librarian/Activities/LibraryActivity.java
+++ EasyLib_Librarian/app/src/main/java/com/easylib/dima/easylib_librarian/Activities/LibraryActivity.java
@@ -1,7 +1,15 @@
 package com.easylib.dima.easylib_librarian.Activities;
 
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
+import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
@@ -11,20 +19,36 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.easylib.dima.easylib_librarian.Adapters.ImageTitleBookAdapter;
+import com.easylib.dima.easylib_librarian.Adapters.ImageTitleEventAdapter;
 import com.easylib.dima.easylib_librarian.Adapters.ImageTitleNewsAdapter;
+import com.easylib.dima.easylib_librarian.ConnectionLayer.ConnectionService;
+import com.easylib.dima.easylib_librarian.ConnectionLayer.Constants;
 import com.easylib.dima.easylib_librarian.R;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Objects;
+import java.util.Set;
 
 import AnswerClasses.Book;
 import AnswerClasses.Event;
 import AnswerClasses.LibraryDescriptor;
 import AnswerClasses.News;
+import AnswerClasses.User;
 
 public class LibraryActivity extends AppCompatActivity {
 
-    private LibraryDescriptor libraryDescriptor;
+    // Needed
+    private static final String USER_INFO = "User Info";
+    private static final String LOGIN = "Login";
+    private static final String LIBRARY_INFO = "Library Info";
+    private User userInfo;
+    private LibraryDescriptor libraryInfo;
 
     // Layout Components
     private TextView name;
@@ -40,13 +64,75 @@ public class LibraryActivity extends AppCompatActivity {
     private RecyclerView booksRec;
     private Button booksButton;
     private FloatingActionButton scanButton;
+    private Button logoutButton;
 
     RecyclerView.LayoutManager mLayoutManager;
+
+    //Comunication
+    ConnectionService mBoundService;
+    private boolean mIsBound;
+
+    //For the communication Service
+    private ServiceConnection mConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            mBoundService = ((ConnectionService.LocalBinder)service).getService();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mBoundService = null;
+        }
+    };
+
+    public void doBindService() {
+        bindService(new Intent(LoginActivity.this, ConnectionService.class), mConnection,
+                Context.BIND_AUTO_CREATE);
+        mIsBound = true;
+        if(mBoundService!=null){
+            mBoundService.IsBoundable();
+        }
+    }
+
+    private void doUnbindService() {
+        if (mIsBound) {
+            // Detach our existing connection.
+            unbindService(mConnection);
+            mIsBound = false;
+        }
+        unregisterReceiver(mMessageReceiver);
+    }
+
+    //This is the handler that will manager to process the broadcast intent
+    //This has to be created inside each activity that needs it ( almost anyone )
+    private BroadcastReceiver mMessageReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String key = extractKey(intent);
+
+            if ( key.equals(Constants.QUERY_ON_BOOKS)) {
+            }
+        }
+    };
+
+    private String extractKey(Intent intent){
+        Set<String> keySet = Objects.requireNonNull(intent.getExtras()).keySet();
+        Iterator iterator = keySet.iterator();
+        return (String)iterator.next();
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.library);
+
+        userInfo = (User) getIntent().getSerializableExtra(USER_INFO);
+        libraryInfo = (LibraryDescriptor) getIntent().getSerializableExtra(LIBRARY_INFO);
+
+        // Communication
+        doBindService();
+        this.registerReceiver(mMessageReceiver, new IntentFilter (Constants.QUERY_ON_BOOKS));
 
         // get layout components references
         name = (TextView) findViewById(R.id.library_activity_name);
@@ -62,16 +148,31 @@ public class LibraryActivity extends AppCompatActivity {
         booksRec = (RecyclerView) findViewById(R.id.library_activity_books_recycle);
         booksButton = (Button) findViewById(R.id.library_activity_all_books_button);
         scanButton = (FloatingActionButton) findViewById(R.id.library_activity_scan_button);
+        logoutButton = (Button) findViewById (R.id.library_activity_logout_button);
+
+        scanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToScanActivity ();
+            }
+        });
 
         // get 6 elements of each arrayList news, events, books
         int i;
+        int size = 0;
+        // set size based on orientation
+        if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+            size = 6;
+        } else if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            size = 3;
+        }
         ArrayList<News> newsList = new ArrayList<News>();
         ArrayList<Event> eventsList = new ArrayList<Event>();
         ArrayList<Book> booksList = new ArrayList<Book>();
-        for(i=0; i<6; i++) {
-            newsList.add(libraryDescriptor.getLibraryContent().getNews().get(i));
-            eventsList.add(libraryDescriptor.getLibraryContent().getEvents().get(i));
-            booksList.add(libraryDescriptor.getLibraryContent().getBooks().get(i));
+        for(i=0; i<size; i++) {
+            newsList.add(libraryInfo.getLibraryContent().getNews().get(i));
+            eventsList.add(libraryInfo.getLibraryContent().getEvents().get(i));
+            booksList.add(libraryInfo.getLibraryContent().getBooks().get(i));
         }
 
         // setup recycleViews
@@ -95,13 +196,31 @@ public class LibraryActivity extends AppCompatActivity {
         // specify adapters
         ImageTitleNewsAdapter newsAdapter = new ImageTitleNewsAdapter(this, newsList);
         newsRec.setAdapter(newsAdapter);
-        ImageTitleNewsAdapter eventsAdapter = new ImageTitleNewsAdapter(this, eventsList);
+        ImageTitleEventAdapter eventsAdapter = new ImageTitleEventAdapter(this, eventsList);
         eventsRec.setAdapter(eventsAdapter);
-        ImageTitleNewsAdapter booksAdapter = new ImageTitleNewsAdapter(this, booksList);
+        ImageTitleBookAdapter booksAdapter = new ImageTitleBookAdapter(this, booksList);
         booksRec.setAdapter(booksAdapter);
     }
 
-    public void goToScanActivity(View view) {
-        // TODO
+    public void goToScanActivity() {
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.initiateScan();
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        if (scanResult != null) {
+            // handle scan result
+        }
+        // else continue with any other code you need in the method
+    }
+
+    public void logout(View view) {
+        SharedPreferences sp = this.getSharedPreferences(LOGIN, MODE_PRIVATE);
+        sp.edit().clear().apply();
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        doUnbindService ();
+        startActivity(intent);
     }
 }
--- EasyLib_Librarian/app/src/main/java/com/easylib/dima/easylib_librarian/ConnectionLayer/Constants.java
+++ EasyLib_Librarian/app/src/main/java/com/easylib/dima/easylib_librarian/ConnectionLayer/Constants.java
@@ -6,25 +6,25 @@ package com.easylib.dima.easylib_librarian.ConnectionLayer;
  */
 public class Constants {
 
-    static final String GET_ALL_RESERVATIONS_FOR_BOOK = "get_all_reservations_for_book";
-    static final String RESERVED_BOOK_RETURNED = "reserved_book_returned";
-    static final String RESERVED_BOOK_TAKEN = "reserved_book_taken";
-    static final String GET_USER_RATED_BOOKS = "get_user_rated_books";
-    static final String QUERY_ON_BOOKS_ALL_LIBRARIES = "query_on_books_all_libraries";
-    static final String GET_WAITING_LIST_USER = "get_waiting_list_user";
-    static final String GET_USER_RESERVATION = "get_user_reservation";
-    static final String GET_EVENTS = "get_events";
-    static final String GET_NEWS = "get_news";
-    static final String GET_WAITING_LIST_BOOK = "get_waiting_list_book";
-    static final String GET_USER_PREFERENCES = "get_user_preferences";
-    static final String PASSWORD_FORGOT = "password_forgot";
-    static final String USER_LOGIN = "user_login";
-    static final String GET_ALL_LIBRARIES = "get_all_libraries";
-    static final String GET_LIBRARY_INFO = "get_library_info";
-    static final String REGISTER_USER = "register_user";
-    static final String QUERY_ON_BOOKS = "query_on_books" ;
-    static final String GET_LIBRARY_CONN_INFO = "library_conn_info";
-    static final String GET_ALL_BOOKS = "get_all_books";
-    static final String NEW_NOTIFICATION_TOKEN = "new_notification_token";
-    static final String LIBRARIAN_LOGIN = "librarian_login";
+    static public final String GET_ALL_RESERVATIONS_FOR_BOOK = "get_all_reservations_for_book";
+    static public final String RESERVED_BOOK_RETURNED = "reserved_book_returned";
+    static public final String RESERVED_BOOK_TAKEN = "reserved_book_taken";
+    static public final String GET_USER_RATED_BOOKS = "get_user_rated_books";
+    static public final String QUERY_ON_BOOKS_ALL_LIBRARIES = "query_on_books_all_libraries";
+    static public final String GET_WAITING_LIST_USER = "get_waiting_list_user";
+    static public final String GET_USER_RESERVATION = "get_user_reservation";
+    static public final String GET_EVENTS = "get_events";
+    static public final String GET_NEWS = "get_news";
+    static public final String GET_WAITING_LIST_BOOK = "get_waiting_list_book";
+    static public final String GET_USER_PREFERENCES = "get_user_preferences";
+    static public final String PASSWORD_FORGOT = "password_forgot";
+    static public final String USER_LOGIN = "user_login";
+    static public final String GET_ALL_LIBRARIES = "get_all_libraries";
+    static public final String GET_LIBRARY_INFO = "get_library_info";
+    static public final String REGISTER_USER = "register_user";
+    static public final String QUERY_ON_BOOKS = "query_on_books" ;
+    static public final String GET_LIBRARY_CONN_INFO = "library_conn_info";
+    static public final String GET_ALL_BOOKS = "get_all_books";
+    static public final String NEW_NOTIFICATION_TOKEN = "new_notification_token";
+    static public final String LIBRARIAN_LOGIN = "librarian_login";
 }
