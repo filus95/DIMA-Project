c101b8b621fd92c68d4e17940dc19c92ebe2e010
Kostandin Caushi <koci4@live.it>
2019-02-09 11:54:14 +0100

    Fix for Tablet

--- EasyLib/app/src/main/java/com/easylib/dima/easylib/ConnectionLayer/MyApplication.java
+++ EasyLib/app/src/main/java/com/easylib/dima/easylib/ConnectionLayer/MyApplication.java
@@ -3,9 +3,10 @@ package com.easylib.dima.easylib.ConnectionLayer;
 import android.annotation.SuppressLint;
 import android.app.Application;
 import android.content.Context;
+import android.support.multidex.MultiDexApplication;
 
 @SuppressLint("Registered")
-public class MyApplication extends Application {
+public class MyApplication extends MultiDexApplication {
 
     private static Context context;
 
