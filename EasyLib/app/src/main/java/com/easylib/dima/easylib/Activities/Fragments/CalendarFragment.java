5b67d7ed8e19e13ee3bf3c96f6df9b94bf4638e9
Kostandin Caushi <koci4@live.it>
2019-02-10 22:32:27 +0100

    Fixed Calendar Fragment

--- EasyLib/app/src/main/java/com/easylib/dima/easylib/Activities/Fragments/CalendarFragment.java
+++ EasyLib/app/src/main/java/com/easylib/dima/easylib/Activities/Fragments/CalendarFragment.java
@@ -10,6 +10,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.easylib.dima.easylib.Adapters.CalendarAdapter;
 import com.easylib.dima.easylib.R;
@@ -23,6 +24,8 @@ public class CalendarFragment extends Fragment {
     private AnswerClasses.User userInfo;
     private ArrayList<String> dates = new ArrayList<String>();
 
+    private TextView noBooksText;
+
     // Dates RecycleView
     private RecyclerView mRecyclerView;
     private CalendarAdapter mAdapter;
@@ -34,47 +37,53 @@ public class CalendarFragment extends Fragment {
         // Inflate the layout for this fragment
         View root = inflater.inflate(R.layout.fragment_calendar, container, false);
 
-        // Inizialize first item with books to take
-        for (AnswerClasses.Reservation r : reservations) {
-            if (!r.isTaken ()) {
-                dates.add ("To Take");
-                break;
+        noBooksText = (TextView) root.findViewById (R.id.fragment_calendar_nobooks);
+
+        if (reservations.size () != 0) {
+            // Inizialize first item with books to take
+            for (AnswerClasses.Reservation r : reservations) {
+                if (!r.isTaken ()) {
+                    dates.add ("To Take");
+                    break;
+                }
             }
-        }
-        // Fill dates ArrayList with distinct dates
-        for (AnswerClasses.Reservation r : reservations) {
-            Boolean isDateToAdd = true;
-            if (r.isTaken ()) {
-                for (String d : dates) {
-                    if (r.getEnd_res_date ().toString ().replace ("T", "  ").equals (d)) {
-                        isDateToAdd = false;
-                        break;
+            // Fill dates ArrayList with distinct dates
+            for (AnswerClasses.Reservation r : reservations) {
+                Boolean isDateToAdd = true;
+                if (r.isTaken ()) {
+                    for (String d : dates) {
+                        if (r.getEnd_res_date ().toString ().replace ("T", "  ").equals (d)) {
+                            isDateToAdd = false;
+                            break;
+                        }
+                    }
+                    if (isDateToAdd) {
+                        dates.add (r.getEnd_res_date ().toString ().replace ("T", "  "));
                     }
                 }
-                if (isDateToAdd) {
-                    dates.add (r.getEnd_res_date ().toString ().replace ("T", "  "));
-                }
             }
-        }
 
-        // RecycleView setup
-        mRecyclerView = (RecyclerView) root.findViewById(R.id.fragment_calendar_recycle);
-        // improve performance
-        mRecyclerView.setHasFixedSize(true);
-        // used linear layout
-        if((getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) == Configuration.SCREENLAYOUT_SIZE_NORMAL) {
+            // RecycleView setup
+            mRecyclerView = (RecyclerView) root.findViewById (R.id.fragment_calendar_recycle);
+            mRecyclerView.setVisibility (View.VISIBLE);
+            // improve performance
+            mRecyclerView.setHasFixedSize (true);
             // used linear layout
-            mLayoutManager = new LinearLayoutManager(getContext());
-        } else if ((getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) == Configuration.SCREENLAYOUT_SIZE_XLARGE) {
-            // used grid layout
-            mLayoutManager = new GridLayoutManager(getContext(), 2);
+            if ((getResources ().getConfiguration ().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) == Configuration.SCREENLAYOUT_SIZE_NORMAL) {
+                // used linear layout
+                mLayoutManager = new LinearLayoutManager (getContext ());
+            } else if ((getResources ().getConfiguration ().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) == Configuration.SCREENLAYOUT_SIZE_XLARGE) {
+                // used grid layout
+                mLayoutManager = new GridLayoutManager (getContext (), 2);
+            }
+            mRecyclerView.setLayoutManager (mLayoutManager);
+            mRecyclerView.setItemAnimator (new DefaultItemAnimator ());
+            // specify an adapter
+            mAdapter = new CalendarAdapter (getContext (), reservations, dates, userInfo);
+            mRecyclerView.setAdapter (mAdapter);
+        } else {
+            noBooksText.setVisibility (View.VISIBLE);
         }
-        mRecyclerView.setLayoutManager(mLayoutManager);
-        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
-        // specify an adapter
-        mAdapter = new CalendarAdapter(getContext(), reservations, dates, userInfo);
-        mRecyclerView.setAdapter(mAdapter);
-
         return root;
     }
 
